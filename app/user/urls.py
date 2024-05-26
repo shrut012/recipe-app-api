e429049251cd02c7682e1250d9469493818f48be
Shrut <shrutpansuriya@gmail.com>
2024-05-26 12:06:15 +0530

    added token based authentication and API for managing profile

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,5 +1,5 @@
 """
-Ur+RL appings for the user API.
+URL appings for the user API.
 """
 from django.urls import path
 
@@ -9,5 +9,7 @@ from user import views
 app_name = 'user'
 
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view(), name='create')
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
 ]
